50e28b2a231aa0e06720db352cc7ff964af2a2e3
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-28 15:43:49 -0700

    Make data.js, create functioning button to toggle the cooking pot in and out of existence

---
+++ main.js
@@ -0,0 +1,30 @@
+//buttons
+var sideButton = document.querySelector("#side-button")
+var entreeButton = document.querySelector('#entree-button')
+var dessertButton = document.querySelector('#dessert-button')
+var letsCookButton = document.querySelector(".lets-cook-button")
+var addRecipeButton = document.querySelector(".add-recipe-button")
+
+//other
+var cookingSuggestion = document.querySelector(".you-should-make")
+var potImage = document.querySelector('.cook-pot-image')
+var insertRecipe = document.querySelector('.insert-recipe')
+
+
+
+function toggleLetsCook () {
+
+}
+
+
+function toggleHidden() {
+  if (potImage.classList.contains("hidden")) {
+    potImage.classList.remove("hidden");
+    cookingSuggestion.classList.add("hidden");
+    insertRecipe.classList.add("hidden");
+  } else {
+  potImage.classList.add("hidden");
+  cookingSuggestion.classList.remove("hidden");
+  insertRecipe.classList.remove("hidden");
+  }
+}
