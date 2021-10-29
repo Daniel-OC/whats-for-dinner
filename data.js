19e217d9ada26cdc33b865e6c256906eef2b996e
Daniel O'Connell <danieloconnell.maker@gmail.com>
2021-10-28 21:00:13 -0700

    Finish Iteration One

--- data.js
+++ data.js
@@ -28,22 +28,22 @@ var entrees = [
 ];
 
 var desserts = [
-  "Apple Pie"
-  "Lemon Meringue Pie"
-  "Black Forest Cake"
-  "Banana Bread"
-  "Peach Cobbler"
-  "Cheesecake"
-  "Funfetti Cake"
-  "Baklava"
-  "Flan"
-  "Macarons"
-  "Macaroons"
-  "Chocolate Cupcakes"
-  "Pavlova"
-  "Pumpkin Pie"
-  "Key Lime Pie"
-  "Tart Tatin"
-  "Croissants"
+  "Apple Pie",
+  "Lemon Meringue Pie",
+  "Black Forest Cake",
+  "Banana Bread",
+  "Peach Cobbler",
+  "Cheesecake",
+  "Funfetti Cake",
+  "Baklava",
+  "Flan",
+  "Macarons",
+  "Macaroons",
+  "Chocolate Cupcakes",
+  "Pavlova",
+  "Pumpkin Pie",
+  "Key Lime Pie",
+  "Tart Tatin",
+  "Croissants",
   "Eclairs"
 ]
--- main.js
+++ main.js
@@ -9,26 +9,60 @@ var addRecipeButton = document.querySelector(".add-recipe-button")
 var cookingSuggestion = document.querySelector(".you-should-make")
 var potImage = document.querySelector('.cook-pot-image')
 var insertRecipe = document.querySelector('.insert-recipe')
-
+var recipe;
 
 
 function toggleLetsCook () {
-
 }
 
+letsCookButton.addEventListener('click', chooseCourse)
 
-function toggleHidden() {
-  if (potImage.classList.contains("hidden")) {
-    potImage.classList.remove("hidden");
-    cookingSuggestion.classList.add("hidden");
-    insertRecipe.classList.add("hidden");
-  } else {
-  potImage.classList.add("hidden");
-  cookingSuggestion.classList.remove("hidden");
-  insertRecipe.classList.remove("hidden");
+// function toggleHidden() {
+//   if (potImage.classList.contains("hidden")) {
+//     potImage.classList.remove("hidden");
+//     cookingSuggestion.classList.add("hidden");
+//     insertRecipe.classList.add("hidden");
+//   } else {
+//   potImage.classList.add("hidden");
+//   cookingSuggestion.classList.remove("hidden");
+//   insertRecipe.classList.remove("hidden");
+//   }
+// }
+
+function chooseCourse() {
+  if (entreeButton.checked) {
+    recipe = entrees[getRandomIndex(entrees)]
+  }else if (sideButton.checked) {
+    recipe = sides[getRandomIndex(sides)]
+  }else if (dessertButton.checked) {
+    recipe = desserts[getRandomIndex(desserts)]
   }
+  insertRecipe.innerHTML = `<h3 class="insert-recipe">${recipe}</h3>`;
+  hidePot();
+  console.log(recipe)
+
+
 }
 
+function hidePot() {
+  addHidden(potImage);
+  removeHidden(cookingSuggestion);
+  removeHidden(insertRecipe);
+}
+
+function showPot() {
+  removeHidden(potImage);
+  addHidden(cookingSuggestion);
+  addHidden(insertRecipe);
+}
+
+function addHidden(element) {
+  element.classList.add('hidden')
+}
+
+function removeHidden(element) {
+  element.classList.remove('hidden')
+}
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length)
